50fcaa2769ca25e1f8d2f8fe332a80a992116357
nichoth <nichoth@gmail.com>
2015-05-01 11:04:37 -0700

    fix get example

--- example/get.js
+++ example/get.js
@@ -2,4 +2,4 @@ var db = require('level')('/tmp/edit.db');
 var fdb = require('../')(db, { dir: '/tmp/edit.blob' });
 
 var hash = process.argv[2];
-fdb.get(hash).pipe(process.stdout);
+fdb.get(hash, console.log);
